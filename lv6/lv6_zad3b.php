e59d84417fe68662e007a8cf1149eeff83ba99a3
Petra <133807669+pperkov1c@users.noreply.github.com>
2024-11-22 10:11:31 +0100

    Delete lv6/lv6_zad3b.php

--- lv6/lv6_zad3b.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (isset($_GET['ime']) && isset($_GET['prezime'])) {
-    $ime = htmlspecialchars($_GET['ime']);
-    $prezime = htmlspecialchars($_GET['prezime']);
-    echo "<h1>$ime $prezime</h1>";
-} else {
-    echo "<h1>Dogodila se pogreška</h1>";
-}
-?>
